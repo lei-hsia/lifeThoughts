e6e1d9cba870ff47ea08503b4d26fe2c901d0102
LEI XIA <lx2196@columbia.edu>
2019-09-06 15:20:23 -0400

    Update 0906_affinityPropagation_TransferEntropy复习.py

--- 0906_affinityPropagation_TransferEntropy复习.py
+++ 0906_affinityPropagation_TransferEntropy复习.py
@@ -1,1 +1,1 @@
-# In statistics and data mining, affinity propagation(AP) is a clustering algorithm based on the 
+# In statistics and data mining, affinity propagation(AP) is a clustering algorithm 
