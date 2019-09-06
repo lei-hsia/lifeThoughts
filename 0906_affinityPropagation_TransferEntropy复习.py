86d0f8076b14bd5f1d2e8ea53c255c0b76458755
LEI XIA <lx2196@columbia.edu>
2019-09-06 15:52:45 -0400

    Update 0906_affinityPropagation_TransferEntropy复习.py

--- 0906_affinityPropagation_TransferEntropy复习.py
+++ 0906_affinityPropagation_TransferEntropy复习.py
@@ -1,1 +1,19 @@
 # In statistics and data mining, affinity propagation(AP) is a clustering algorithm 
+# based on the concept of "Message passing" between data points. Unlike clustering 
+# algorithms such as k-means or k-medoids, AP does not require the number of clusters
+# to be determined or estimated before running the algorithm.
+# AP聚类算法的核心是找到 最大化:{节点K对节点i的吸引度+节点i归于节点K}, i.e. 
+# max{a(i, k) + s(i, k)}
+
+# The inventor of AP showed that it is better for certain computer vision and computational
+# biology tasks, e.g. clustering of pictures of human faces and identifying regulated 
+# transcripts, than k-means, even when k-means was allowed many random restarts and initialized
+# using PCA. 
+
+# Transfer entropy is a non-parametric statistic measuring the amount of directed transfer
+# of information between 2 random processes. Transfer entropy from a process X to another
+# process Y is the amount of uncertainty reduced in future values of Y by knowing the past
+# values of X given past values of Y.
+
+import sys
+from datetime import datetime
